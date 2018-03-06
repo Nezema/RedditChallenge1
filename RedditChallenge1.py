6187d3a0f9de2fa92dce72721a4e139ad2d1b12a
Nezema <37089225+Nezema@users.noreply.github.com>
2018-03-05 16:31:00 -0800

    Finished the problems

---
+++ RedditChallenge1.py
@@ -0,0 +1,7 @@
+Name = input("What is your name?")
+age = input("How old are you?")
+UserName = input("What is your Reddit Username?")
+
+print("Your name is", Name)
+print("Your age is", age)
+print("Your Username is", UserName)
