bfbdb39aaa74d99cefd178d8f5d1f52d3e62a8f1
Nezema <37089225+Nezema@users.noreply.github.com>
2018-03-05 16:05:41 -0800

    Added the python file

---
+++ python.py
@@ -0,0 +1,7 @@
+Name = raw_input("What is your name?")
+age  = input("How old are you?")
+UserName = raw_input("What is your Reddit Username?")
+
+print ("Your name is"), Name
+print ("Your age is"), age
+print ("Your Username is"), UserName
